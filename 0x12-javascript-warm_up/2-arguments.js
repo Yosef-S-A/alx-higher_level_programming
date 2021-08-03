788becf8d4360153520d3a53942639084f4a1bd7
Yosef-S-A <yosefsamuel22@gmail.com>
2021-08-03 06:04:44 +0000

    added else if to 2-argments.js script

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -5,8 +5,10 @@
 
 const myArgs = process.argv.slice(2);
 
-if (myArgs.length) {
+if (myArgs.length === 1) {
   console.log('Argument found');
+} else if (myArgs.length > 1) {
+  console.log('Arguments found');
 } else {
   console.log('No argument');
 }
