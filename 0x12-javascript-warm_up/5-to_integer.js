32870891fa235c7c70f5d0b65b3374a01b114b05
Yosef-S-A <yosefsamuel22@gmail.com>
2021-08-03 06:08:21 +0000

    corrected the semi-standard compatablity of 5-to_integer.js script

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -6,8 +6,7 @@
 const args = process.argv.slice(2);
 
 if (Number.isNaN(parseInt(args[0]))) {
-    console.log('Not a number');
-}
-else {
-    console.log('My number: ' +  parseInt( args[0]));
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + parseInt(args[0]));
 }
