a0d54039a75cfcd32c3bc09c0dbb6471f8fb8bc4
Yosef-S-A <yosefsamuel22@gmail.com>
2021-08-03 07:02:48 +0000

    semistandard compatebility

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,24 +1,20 @@
 #!/usr/bin/node
 // script that prints the addition of 2 integers
 
-let result = 1;
-
 const args = process.argv.slice(2);
 
 const firstArgValue = parseInt(args[0]);
 
 if (Number.isNaN(firstArgValue)) {
   console.log(1);
-}
-else {
+} else {
   console.log(factorial(firstArgValue));
 }
 
 function factorial (n) {
-    if (n === 0 || n === 1){
-	return 1;
-    }
-    else{
-	return n * factorial(n-1);
-    }
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
 }
