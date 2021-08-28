e524578040ce7ec229d7fbe4627f37c254ba6bd0
Yosef-S-A <yosefsamuel22@gmail.com>
2021-08-28 21:08:25 +0000

    task 8: uppercase converter function

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,10 +3,8 @@
 
 def uppercase(str):
     '''function that converts a string to uppercase'''
-    upper_str = ""
     for x in str:
         if ord('a') <= ord(x) <= ord('z'):
-            upper_str += chr(65 + (ord(x) - ord('a')))
-        else:
-            upper_str += x
-    print(upper_str)
+            x = chr(65 + (ord(x) - ord('a')))
+        print(x, end="")
+    print("")
