a30aea6ab96ba6e366b30b87653251352ba0344c
Yosef-S-A <yosefsamuel22@gmail.com>
2021-08-28 18:49:49 +0000

    task 7

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,14 +1,5 @@
 #!/usr/bin/python3
 
-
-#def islower(c):
-#    return(ord('a') <= ord(c) <= ord('z'))
-
-
 def islower(c):
-    '''This function checks for lowercase or uppercase of a character'''
-    if ord(c) in range(97, 123):
-        return True
-    else:
-        return False
-
+        return(ord('a') <= ord(c) <= ord('z'))
+        
